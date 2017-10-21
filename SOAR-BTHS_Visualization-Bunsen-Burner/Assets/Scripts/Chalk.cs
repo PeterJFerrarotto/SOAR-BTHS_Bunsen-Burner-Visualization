72c7f0b87016acec46ba29ba74f4b4887a28a546
Unknown <peterjferrarotto@gmail.com>
2017-10-21 11:23:19 -0400

    Added unity files

---
+++ SOAR-BTHS_Visualization-Bunsen-Burner/Assets/Scripts/Chalk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chalk : MonoBehaviour {
+    public GameObject soot;
+
+    private int particleCollCount;
+    public int particleCollCutoff;
+
+	// Use this for initialization
+	void Start () {
+        particleCollCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        soot.SetActive(particleCollCount >= particleCollCutoff);
+	}
+
+    private void OnDisable()
+    {
+        soot.SetActive(false);
+        particleCollCount = 0;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        particleCollCount++;   
+    }
+}
---
+++ SOAR-BTHS_Visualization-Bunsen-Burner/Assets/Scripts/VisualizationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualizationManager : MonoBehaviour {
+    public Text intakeRateVal;
+    public BunsenBurner burner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        intakeRateVal.text = (burner.AirIntakeRate * 100).ToString("0");
+	}
+}
